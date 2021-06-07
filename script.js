aab685951a0c3ae532bfb02f97fc9eb29c0c97b5
shsmink <79309340+shsmink@users.noreply.github.com>
2021-06-07 09:53:51 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+
+ikRockArrowFunctions();
+
+const fivePlusSeven = () => 5 + 7;
+  
+fivePlusSeven();
+
+const myFunction = (a, b) => a + b;
+  
+myFunction(5, 7);
+
+const addFive = a => a + 5;
+  
+addFive(7);
+
+const createObject = () => ({greeting: "hoi"});
+
+createObject();
